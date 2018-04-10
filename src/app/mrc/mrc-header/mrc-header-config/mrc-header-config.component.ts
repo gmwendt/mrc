ffcf474990a670792cf6864fdd03809a980e5fee
gmwendt <marcon@elipse.com.br>
2018-04-10 17:22:24 -0300

    commit 100418

--- src/app/mrc/mrc-header/mrc-header-config/mrc-header-config.component.ts
+++ src/app/mrc/mrc-header/mrc-header-config/mrc-header-config.component.ts
@@ -3,9 +3,32 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'mrc-header-config',
 	templateUrl: './mrc-header-config.component.html',
-	// styleUrls: ['./page-products.component.css'],
+	styleUrls: ['./mrc-header-config.component.css'],
 	encapsulation: ViewEncapsulation.None
 })
 export class MrcHeaderConfigComponent { 
-
+	private _filesExtensions: string = '.xml';
+	
+	private files_imported(files: FileList): void {
+		if (files.length < 1)
+      return;
+		
+		for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+			this.readFileAsync(file).then(result => {
+				//TODO
+				console.log(result);
+			});
+		}
+	}
+	
+	private async readFileAsync(file: File): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      var reader = new FileReader();
+      reader.onloadend = function () {
+        resolve(reader.result);
+      };
+      reader.readAsText(file);
+    });
+  }
 }
